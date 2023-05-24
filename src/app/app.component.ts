d0ce44f562c8e0fd7a585b380fed00adbfba5ad8
Elag1988 <acevedoedgard1988@msn.com>
2023-05-24 17:37:18 -0500

    Creacion del repositorio con el ejercicio donde se pide mostrar un array de Productos como una tabla a traves de property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { SelectorNumericoComponent } from './selector-numerico/selector-numerico.component';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'selector-numerico';
+  @ViewChild("selector2") selector2: any|SelectorNumericoComponent
+  fijarSelector2(valor:number) {
+
+    this.selector2.fijar(valor)
+
+  }
+
+  articulos: any[] = [
+    { id: 1, producto: 'Vestido con flores estampadas',color: "Rojo",talla: "M", precio: 120000 },
+    { id: 2, producto: 'Vestido con rayas negras',color:"Blanco",talla: "S", precio: 110000 },
+    { id: 3, producto: 'Blusa con estampado de anime', color: "Morada",talla: "L", precio: 60000 },
+    { id: 4, producto: 'Pantalon de mezcilla', color: "Azul Marino",talla: "30", precio: 100000 },
+    { id: 5, producto: 'Falda Larga',color: "negro",talla: "32", precio: 90000 }
+  ];
+
+
 }
+
+
+//En la componente AppComponent definir un arreglo con los datos de 5 artículos. Luego crear una segunda componente llamada 'listadoarticulos' que reciba mediante una propiedad el arreglo y su objetivo sea mostrar en una tabla HTML todos los datos.usando enlace de propiedades (Property Binding)
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SelectorNumericoComponent } from './selector-numerico/selector-numerico.component';
+import { ListadoarticulosComponent } from './listado-articulos/listadoarticulos/listadoarticulos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SelectorNumericoComponent,
+    ListadoarticulosComponent
   ],
   imports: [
     BrowserModule,
